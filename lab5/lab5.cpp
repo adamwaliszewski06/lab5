c0413643986f9ffcc3ddca6b9e3de8325ebe51b4
stanislawprycinski <prycinskis@gmail.com>
2025-11-04 14:00:25 +0100

    Add project files.

---
+++ lab5/lab5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream> 
+using namespace std;
+
+/* zad 1 - wprowadzenie 10 wartosci do tabeli i wyswietlenie w odwrotnej kolejnosci */
+int main() { 
+    int tab[10];
+    int n;
+
+    for (int i = 0; i < 10; i++) { //pozycje od 0 do 9
+        cout << "Wprowadz " << i+1 << " liczbe: ";
+        cin >> n;
+        tab[i] = n; 
+    }
+
+    for (int i = 9; i >= 0; i=i-1) { //pozycje od 9 do 0 wlacznie
+        cout << tab[i] << endl;
+    }
+    return 0;
+}
