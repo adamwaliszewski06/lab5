e13f45c44f6d242eb24238bd008617fdd0ed0896
stanislawprycinski <prycinskis@gmail.com>
2025-11-04 14:49:41 +0100

    zad 4

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -51,7 +51,7 @@ int main() {
 /* zad 3 - Napisz program, który wygeneruje tablicę i wypełni ją 10 losowymi elementami typu int. Następnie:
  1. Oblicz wartość średnią liczb znajdujących się w tablicy i wypisz ile jest elementów mniejszych od obliczonej
  wartości średniej.
- 2. Znajdź element najmniejszy i największy w tablicy */
+ 2. Znajdź element najmniejszy i największy w tablicy 
 
 float srednia(int tab[], int rozmiar);
 int maksimum(int tab[], int rozmiar);
@@ -65,20 +65,15 @@ int main() {
     int tab[10];
 
     for (int i = 0; i < 10; ++i) {
-        tab[i] = dist(gen);
-    }
+        tab[i] = dist(gen);}
 
     cout << "Wygenerowane 10 losowych liczb:" << endl;
     for (int i = 0; i < 10; ++i) {
-        cout << "tab[" << i << "] = " << tab[i] << endl;
-    }
+        cout << "tab[" << i << "] = " << tab[i] << endl;}
 
     cout << "Srednia wartosc liczb w tablicy wynosi: " << srednia(tab, 10) << endl;
 	cout << "Najmniejszy element w tablicy to: " << minimum(tab, 10) << endl;
 	cout << "Najwiekszy element w tablicy to: " << maksimum(tab, 10) << endl;
-
-
-
 }
 
 float srednia(int tab[], int rozmiar) {
@@ -86,25 +81,57 @@ float srednia(int tab[], int rozmiar) {
     for (int i = 0; i < rozmiar; ++i) {
         suma += tab[i];
     }
-    return suma / rozmiar;
-}
+    return suma / rozmiar;}
 
 int maksimum(int tab[], int rozmiar) {
     int maks = tab[0];
     for (int i = 1; i < rozmiar; ++i) {
         if (tab[i] > maks) {
-            maks = tab[i];
-        }
-    }
-    return maks;
-}
+            maks = tab[i];}}
+    return maks;}
 
 int minimum(int tab[], int rozmiar) {
     int min = tab[0];
     for (int i = 1; i < rozmiar; ++i) {
         if (tab[i] < min) {
-            min = tab[i];
+            min = tab[i];}}
+    return min;}
+    */
+
+/* zad 4 -  Napisz program, który prosi użytkownika o podanie wymiaru macierzy (użytkownik określa liczbę wierszy
+ i kolumn), następnie program prosi użytkownika o wprowadzenie wartości dla poszczególnych komórek zadanej
+ macierzy i wyświetla ją 
+
+int main() {
+    int** macierz;
+	int w, k; // liczby wierszy i kolumn
+	cout << "Podaj liczbe wierszy macierzy: ";
+	cin >> w;
+	cout << "Podaj liczbe kolumn macierzy: ";   
+	cin >> k;
+    macierz = new int* [w]; //przydzielenie pamięci na okreslona liczbe wierszy
+
+    for (int i = 0; i < w; i++)
+        macierz[i] = new int[k]; //przydzielenie dla każdego wiersza po k komórek
+
+	for (int i = 0; i < w; i++) { //dla kazdego wiersza
+		for (int j = 0; j < k; j++) { //dla kazdej kolumny
+            cout << "Podaj element macierzy na pozycji [" << i << "][" << j << "]: ";
+            cin >> macierz[i][j];
+		}
         }
+    cout << "Wprowadzona macierz:  " << endl;
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < k; j++) {
+            cout << macierz[i][j] << " ";
+        }  
+		cout << endl; //po wypisaniu kazdego wiersza przechodzi do nowej linii
     }
-    return min;
+	return 0;
 }
+*/
+
+/* zad 5 - Napisz program, który pozwala użytkownikowi na wprowadzenie n liczb rzeczywistych do tablicy 10
+elementowej, gdzie n jest z zakresu [0,10]. Jeśli użytkownik wprowadzi mniej niż 10 liczb, to dopełnij tablicę
+ zerami (przykładowo jeśli użytkownik poda 4 liczby, to 6 pozostałych elementów tablicy przyjmuje wartość 0).
+ Dla tak przygotowanej tablicy oblicz wartość średnią, maksymalną i minimalną */
